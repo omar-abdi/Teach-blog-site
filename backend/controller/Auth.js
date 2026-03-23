c5712329e0e5a56609dcc4d453614de1b85247fb
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-03-23 07:46:58 -0700

    9.logout function

--- backend/controller/Auth.js
+++ backend/controller/Auth.js
@@ -121,3 +121,19 @@ export const Login = async(req , res)=>{
     
    }
 }
+
+ export     const lougOut = (req , res)=>{
+   try {
+     res.clearCookie("access-token")
+     res.status(200).json({
+        error: "successfully lougout"
+     })
+   } catch (error) {
+     console.log("Error " ,error.message)
+    res.status(500).json({
+        message: error.message
+    })
+
+    
+   }
+}
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
-import { RegisterUser , Login } from "../controller/Auth.js"
+import { RegisterUser , Login , lougOut } from "../controller/Auth.js"
 
 
 const router = Router()
 router.post("/signup" , RegisterUser)
 router.post("/login" , Login)
+router.post("/logout" , lougOut)
 
 export default router
