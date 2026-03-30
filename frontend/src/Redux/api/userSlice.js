f660fddca615a01d0b61cc7c8fe46095e005e571
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-03-30 02:56:54 -0700

    25.Signup function

--- frontend/src/Redux/api/userSlice.js
+++ frontend/src/Redux/api/userSlice.js
@@ -1,7 +1,7 @@
-import { createSlice   , createAsyncThunk } from "@reduxjs/toolkit"
+import { createSlice   , createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit"
 
 
-export const Login = createAsyncThunk("user/login" , async(userInfo)=>{
+export const Login = createAsyncThunk("user/login" , async(userInfo , {rejectWithValue})=>{
     const response = await fetch("api/users/login" ,{
         method :"POST",
       headers: {
@@ -10,9 +10,28 @@ export const Login = createAsyncThunk("user/login" , async(userInfo)=>{
 body: JSON.stringify(userInfo)
 
     })
-    return await response.json()
+   const data = await res.json()
+    if(!response.ok || data.error){
+           return rejectWithValue(data.error || "login is failed")
+    }
+    return data
 
 
+})
+export const Register = createAsyncThunk("user/signup" , async(userInfo, {rejectWithValue})=>{
+    const res = await fetch("api/users/signup" ,{
+        method :"POST",
+      headers: {
+    "Content-Type": "application/json"
+},
+
+body: JSON.stringify(userInfo)
+    })
+const data = await res.json()
+    if(!res.ok || data.error){
+        return rejectWithValue(data.error || "Registration is failed")
+    }
+    return data
 })
 const getInitialState = ()=>{
     if(typeof window !== "undefined"){
@@ -48,6 +67,17 @@ export const userSlice = createSlice({
             state.currentUser= action.payload
             localStorage.setItem("user" , JSON.stringify(action.payload))
         }).addCase(Login.rejected , (state , action)=>{
+            state.status = "failed",
+            state.error = action.error.message
+        }).addCase(Register.pending , (state)=>{
+            state.status= "loading",
+            state.error = null
+
+        }).addCase(Register.fulfilled , (state , action)=>{
+            state.status = "success",
+            state.currentUser= action.payload
+            localStorage.setItem("user" , JSON.stringify(action.payload))
+        }).addCase(Register.rejected , (state , action)=>{
             state.status = "failed",
             state.error = action.error.message
         })
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import { Register } from '../Redux/api/userSlice'
+import { useDispatch } from 'react-redux'
 function Signup() {
+  const dispatch = useDispatch()
+  const [formData , setFormData] = useState({
+    username :"",
+    fullName : "",
+    email : "",
+    password : ""
+  })
+   const handleChanle = (e)=>{
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+      })
+    }
+    const handlSubmit =(e)=>{
+    e.preventDefault()
+    dispatch(Register(formData))
+  
+    }
   return (
     <div className='container  px-4 mx-auto max-w-4xl  overflow-hidden flex flex-col md:flex-row justify-around py-20'>
       <div>
@@ -10,33 +29,33 @@ function Signup() {
       </div>
       <div>
     <h1 className='text-2xl font-semibold  text-slate-700'>Create Account</h1>
-         <form className='text-slate-800 flex flex-col gap-5   text-slate-800' >
+         <form  onSubmit={handlSubmit} className='text-slate-800 flex flex-col gap-5   text-slate-800' >
 
             <div >
                 <label  className='block my-2'>fullNmae:</label>
                 <div>
-                    <input type="text"  placeholder='Enter fullName' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
+                    <input type="text"   name='fullName' value={formData.fullName} onChange={handleChanle} placeholder='Enter fullName' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
                 </div>
 
             </div>
             <div >
                 <label  className='block my-2'>Username:</label>
                 <div>
-                    <input type="text"  placeholder='Enter Username' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
+                    <input type="text"  name='username' value={formData.username} onChange={handleChanle}    placeholder='Enter Username' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
                 </div>
 
             </div>
             <div >
                 <label  className='block mb-2 text-sm font-medium'>Email:</label>
                 <div>
-                    <input type="email"  placeholder='Enter Email' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
+                    <input type="email" name='email' value={formData.email} onChange={handleChanle}   placeholder='Enter Email' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
                 </div>
 
             </div>
             <div >
                 <label  className='block mb-2 text-sm font-medium'>password:</label>
                 <div>
-                    <input type="password"  placeholder='Enter Username' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
+                    <input type="password" name='password' value={formData.password} onChange={handleChanle}   placeholder='Enter password' className='w-full p-3 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black transition'/>
                 </div>
 
             </div>
