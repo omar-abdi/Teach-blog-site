a20e824880f4bdbb57233ce6f88a9db253716fe3
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-04-02 14:51:57 -0700

    33.create-post-ui

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
-
+import { useRef , useState } from 'react'
+import "react-quill-new/dist/quill.snow.css"
+import ReactQuill from 'react-quill-new'
 function CreatePost() {
+  const [image , setImage] = useState(null)
+  const [formData , setFormData] = useState({
+    image : ""
+  })
+  const inputRefImage = useRef()
+  const handdleFIleChange = (e)=>{
+    const file = e.target.files[0]
+    if(file){
+      const reader = new FileReader()
+      reader.onload  = ()=>{
+        setImage(reader.result)
+        setFormData((prev)=>({
+       ...prev,
+       image: reader.result
+        }))
+      }
+     reader.readAsDataURL(file)
+    }
+  }
   return (
-    <div>
-      create post
+    <div className='p-4 flex-1 max-w-4xl mx-auto'>
+  <div className='space-y-2 mb-4 '>
+    <h1 className='text-2xl text-slate-800 font-bold'>Your Posts</h1>
+    <p>Share post with your friends</p>
+  </div>
+  {/* fromka inputs */}
+  <form className='space-y-6' > 
+    <div className='space-y-2' >
+      {/* input title */}
+<label  className='block text-sm font-medium text-gray-800' htmlFor='Title'>Titel:</label>
+<input type="text"  placeholder='Enter Title Your post' className='w-full rounded-lg px-4 py-2 text-slate-700 border border-slate-300' />
+    </div>
+    <div className='space-y-2' >
+      {/* category input */}
+<label  className='block text-sm font-medium text-gray-800' htmlFor='category'>Categroy:</label>
+<input type="text"  placeholder='Category post' className='w-full rounded-lg px-4 py-2 text-slate-700 border border-slate-300' />
+    </div>
+    <div className='space-y-2' >
+      {/* image input */}
+<label  className='block text-sm font-medium text-gray-800' htmlFor='category'>image:</label>
+<div>
+  <input type="file"   onChange={handdleFIleChange} className='hidden' id='file' ref={inputRefImage} accept='image/*' />
+  <img src= {image ? image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYWJgwzbZmDbken0mXjc4idbPF-3HRilVQvw&s'} onClick={()=>inputRefImage.current.click()}  alt="" className='h-[100px] '  />
+</div>
+    </div>
+    <div className='space-y-2' >
+      {/* category input */}
+<label  className='block text-sm font-medium text-gray-800' htmlFor='category'>Content :</label>
+<ReactQuill placeholder='Your post Content' className='mb-12'/>
+    </div>
+    <button className='w-full md:w-1/2 bg-gray-700 shadow-lg rounded-lg px-6 py-3 text-white hover:bg-gray-900'>Post </button>
+  </form>
     </div>
   )
 }
